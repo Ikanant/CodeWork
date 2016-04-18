66b3bdf2de02136e18df1ab5d274d41cce5da64d
Ikanant <jonathan.hdez92@gmail.com>
2016-04-18 10:02:04 -0400

    Enhanced answers for CyclingRotation

--- CyclingRotation/util.java
+++ CyclingRotation/util.java
@@ -1,27 +1,90 @@
 public class util {
-	public static void main(String[] args){
-		int A [] = {1,2,3,4,5};
-		int B [] = solution(A, 6);
+	public static void main(String[] args) {
+		int A[] = { 1, 2, 3, 4, 5 };
+		int B[] = solution1(A, 3);
 
-		for(int e: B){
+		for (int e : B) {
 			System.out.println(e);
 		}
 	}
 
-	public static int[] solution(int[] A, int K) {
-        int B [] = A.clone();
+	// BEST answer
+	public static int[] solution1(int[] A, int K) {
 
-        int shiftLocation;
-        for(int index=0; index<A.length; index++){
-        	shiftLocation = index+K;
+		// 1 2 3 4 5
+		//
+		// 1 -
+		// 1 2 3 4 5
+		// 1 3
+		// 1 2 1 4 5
+		// 3 5
+		// 1 2 1 4 3
+		// 5 2
+		// 1 5 1 4 3
+		// 2 4
+		// 1 5 1 2 3
+		// 4 1
+		// 4 5 1 2 3
 
-    		while(shiftLocation > A.length-1){
-    			shiftLocation -= A.length;
-    		}
+		int tempOne = A[0];
+		int tempTwo;
+		int swapIndex = K;
+		for (int i = 0; i < A.length; i++) {
+			while (swapIndex >= A.length) {
+				swapIndex -= A.length;
+			}
 
-            B[shiftLocation] = A[index];
-        }
+			tempTwo = A[swapIndex];
 
-        return B;
-    }
+			A[swapIndex] = tempOne;
+
+			tempOne = tempTwo;
+
+			swapIndex += K;
+		}
+
+		return A;
+	}
+
+	// Good track answer but not perfect
+	public static int[] solution2(int[] A, int K) {
+
+		while (K != 0) {
+			int tempOne = A[0];
+			int tempTwo;
+			for (int i = 1; i <= A.length; i++) {
+				if (i != A.length) {
+					tempTwo = A[i];
+					A[i] = tempOne;
+				} else {
+					tempTwo = A[0];
+					A[0] = tempOne;
+				}
+
+				tempOne = tempTwo;
+
+			}
+			K--;
+		}
+
+		return A;
+	}
+
+	// Cheat Answer
+	public static int[] solution3(int[] A, int K) {
+		int B[] = A.clone();
+
+		int shiftLocation;
+		for (int index = 0; index < A.length; index++) {
+			shiftLocation = index + K;
+
+			while (shiftLocation > A.length - 1) {
+				shiftLocation -= A.length;
+			}
+
+			B[shiftLocation] = A[index];
+		}
+
+		return B;
+	}
 }
