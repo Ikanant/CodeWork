a12a893358143177351353b8560f8428d970a55e
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-05-15 01:35:12 -0500

    Bug fixed and further enhanced performance

--- CountDiv/util.java
+++ CountDiv/util.java
@@ -1,16 +1,9 @@
 class util {
   public static void main(String[] args) {
-    System.out.println(solution(10,11,2));
+    System.out.println(solution(2,12,3));
   }
 
   public static int solution(int A, int B, int K){
-    int count = (B-A)/K;
-
-    if (count>1){
-      return 1 + count;
-    } else {
-      return -1;
-    }
-
+    return (A%K==0) ? (B/K - A/K) + 1 : (B/K - A/K);
   }
 }
