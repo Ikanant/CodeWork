78e96c27160e3b17dcbd3fbcb04069b1a41fa212
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-05-18 01:35:12 -0500

    Swap variables without TEMP using XOR logic

--- SwapVar/util.java
+++ SwapVar/util.java
@@ -7,7 +7,17 @@ class util {
 		a = a+b;
 		b = a-b;
 		a = a-b;
-
 		System.out.println("a: " + a + "\nb: " + b);
+		//OR
+
+		int x = 1;
+		int y = 2;
+
+		//Swap:
+		x ^= y;
+		y ^= x;
+		x ^= y;
+
+		System.out.println("x: " + x + "\ny: " + y);
 	}
 }
